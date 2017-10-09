43ebf69f386a49045a4a431345d7715c0fb32c9f
pujunwu <juwu_email@163.com>
2017-10-09 11:23:08 +0800

    8.0

--- permissionlibrary/src/main/java/com/junwu/permission/PermissionParam.java
+++ permissionlibrary/src/main/java/com/junwu/permission/PermissionParam.java
@@ -2,6 +2,7 @@ package com.junwu.permission;
 
 import android.Manifest;
 import android.content.Context;
+import android.os.Build;
 
 /**
  * ===============================
@@ -17,7 +18,6 @@ public class PermissionParam {
     String message = "为了应用可以正常使用，请您点击确认申请权限。";
     String negativeButton = "取消";
     String psitiveButton = "确定";
-    //    boolean needGotoSetting = false;// 如果权限申请有失败的，判断是否跳转到权限设置界面
     boolean isShowDialog = false;//如果进入系统权限管理界面后，权限还未获取成功，就提示是否提示重新获取
 
     public PermissionParam setContext(Context context) {
@@ -50,11 +50,6 @@ public class PermissionParam {
         return this;
     }
 
-//    public PermissionParam setNeedGotoSetting(boolean needGotoSetting) {
-//        this.needGotoSetting = needGotoSetting;
-//        return this;
-//    }
-
     public PermissionParam setShowDialog(boolean showDialog) {
         isShowDialog = showDialog;
         return this;
@@ -77,40 +72,144 @@ public class PermissionParam {
 
     public static PermissionParam getParamCamera() {
         return new PermissionParam()
-                .setPermissions(Manifest.permission.CAMERA);
+                .setPermissions(CAMERA);
     }
 
     public static PermissionParam getParamSDCard() {
         return new PermissionParam()
-                .setPermissions(Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
-    }
-
-    public static PermissionParam getParamCameraSDCard() {
-        return new PermissionParam()
-                .setPermissions(Manifest.permission.CAMERA,
-                        Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
-    }
-
-    public static PermissionParam getParamWifi() {
-        return new PermissionParam()
-                .setPermissions(Manifest.permission.CHANGE_WIFI_STATE,
-                        Manifest.permission.ACCESS_WIFI_STATE,
-                        Manifest.permission.ACCESS_NETWORK_STATE);
+                .setPermissions(STORAGE);
     }
 
     public static PermissionParam getParamVideo() {
         return new PermissionParam()
-                .setPermissions(Manifest.permission.CAMERA,
-                        Manifest.permission.RECORD_AUDIO,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                .setPermissions(VIDEO);
     }
+
+    public static PermissionParam getParamMicrophone() {
+        return new PermissionParam()
+                .setPermissions(MICROPHONE);
+    }
+
+//    public static PermissionParam getParamCalendar() {
+//        return new PermissionParam()
+//                .setPermissions(Manifest.permission.READ_CALENDAR,
+//                        Manifest.permission.WRITE_CALENDAR);
+//    }
+//
+//    public static PermissionParam getParamContacts() {
+//        return new PermissionParam()
+//                .setPermissions(Manifest.permission.READ_CONTACTS,
+//                        Manifest.permission.WRITE_CONTACTS,
+//                        Manifest.permission.GET_ACCOUNTS);
+//    }
+//
+//    public static PermissionParam getParamLocation() {
+//        return new PermissionParam()
+//                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION,
+//                        Manifest.permission.ACCESS_COARSE_LOCATION);
+//    }
+//
+//    public static PermissionParam getParamPhone() {
+//        return new PermissionParam()
+//                .setPermissions(Manifest.permission.READ_PHONE_STATE,
+//                        Manifest.permission.CALL_PHONE,
+//                        Manifest.permission.READ_CALL_LOG,
+//                        Manifest.permission.WRITE_CALL_LOG,
+//                        Manifest.permission.USE_SIP,
+//                        Manifest.permission.PROCESS_OUTGOING_CALLS);
+//    }
+//
+//    public static PermissionParam getParamSensors() {
+//        return new PermissionParam()
+//                .setPermissions(Manifest.permission.BODY_SENSORS);
+//    }
+//
+//    public static PermissionParam getParamSMS() {
+//        return new PermissionParam()
+//                .setPermissions(Manifest.permission.SEND_SMS,
+//                        Manifest.permission.RECEIVE_SMS,
+//                        Manifest.permission.READ_SMS,
+//                        Manifest.permission.RECEIVE_WAP_PUSH,
+//                        Manifest.permission.RECEIVE_MMS);
+//    }
 
     private static PermissionParam getParam(String... permissions) {
         return new PermissionParam().setPermissions(permissions);
     }
 
     //更多权限组说明：http://blog.csdn.net/koma025/article/details/52913511
+
+    /****************************8.0权限组**************************/
+
+    public static final String[] CALENDAR;   // 读写日历。
+    public static final String[] CAMERA;     // 相机。
+    public static final String[] CONTACTS;   // 读写联系人。
+    public static final String[] LOCATION;   // 读位置信息。
+    public static final String[] MICROPHONE; // 使用麦克风。
+    public static final String[] PHONE;      // 读电话状态、打电话、读写电话记录。
+    public static final String[] SENSORS;    // 传感器。
+    public static final String[] SMS;        // 读写短信、收发短信。
+    public static final String[] STORAGE;    // 读写存储卡。
+    public static final String[] VIDEO;//录像
+
+    static {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            CALENDAR = new String[]{};
+            CAMERA = CALENDAR;
+            CONTACTS = CALENDAR;
+            LOCATION = CALENDAR;
+            MICROPHONE = CALENDAR;
+            PHONE = CALENDAR;
+            SENSORS = CALENDAR;
+            SMS = CALENDAR;
+            STORAGE = CALENDAR;
+            VIDEO = CALENDAR;
+        } else {
+            CALENDAR = new String[]{
+                    Manifest.permission.READ_CALENDAR,
+                    Manifest.permission.WRITE_CALENDAR};
+
+            CAMERA = new String[]{
+                    Manifest.permission.CAMERA};
+
+            CONTACTS = new String[]{
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.WRITE_CONTACTS,
+                    Manifest.permission.GET_ACCOUNTS};
+
+            LOCATION = new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION};
+
+            MICROPHONE = new String[]{
+                    Manifest.permission.RECORD_AUDIO};
+
+            PHONE = new String[]{
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.CALL_PHONE,
+                    Manifest.permission.READ_CALL_LOG,
+                    Manifest.permission.WRITE_CALL_LOG,
+                    Manifest.permission.USE_SIP,
+                    Manifest.permission.PROCESS_OUTGOING_CALLS};
+
+            SENSORS = new String[]{
+                    Manifest.permission.BODY_SENSORS};
+
+            SMS = new String[]{
+                    Manifest.permission.SEND_SMS,
+                    Manifest.permission.RECEIVE_SMS,
+                    Manifest.permission.READ_SMS,
+                    Manifest.permission.RECEIVE_WAP_PUSH,
+                    Manifest.permission.RECEIVE_MMS};
+
+            STORAGE = new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+            VIDEO = new String[]{Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        }
+    }
 
 }
